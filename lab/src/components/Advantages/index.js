d1a82e07bdace927978385d53f9b0b81692de0f3
Khess S Dotto <khessdotto@gmail.com>
2023-01-31 08:50:11 -0300

    First page completed. Second page left.

---
+++ lab/src/components/Advantages/index.js
@@ -0,0 +1,13 @@
+import icon1 from "../../images/icon1.png"
+import icon2 from "../../images/icon1.png"
+import icon3 from "../../images/icon1.png"
+import icon4 from "../../images/icon1.png"
+
+const title1 = ;
+const text1 = ;
+
+const title2 = ;
+const text2 = ;
+
+const title3 = ;
+const text3 = ;
